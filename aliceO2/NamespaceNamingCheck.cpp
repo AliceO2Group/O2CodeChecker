a2adf747517ae0a06df84de57eb87516b05db7cb
Zdravko Donev <kfzzdravko@gmail.com>
2017-09-19 16:39:58 +0200

    Adding handling of more use cases for namespace naming (#20)
    improved way of fixing wrong namespaces

--- aliceO2/NamespaceNamingCheck.cpp
+++ aliceO2/NamespaceNamingCheck.cpp
@@ -13,31 +13,46 @@
 #include <regex>
 #include <string>
 #include <ctype.h>
-
+ 
 using namespace clang::ast_matchers;
 
 namespace clang {
 namespace tidy {
 namespace aliceO2 {
+ 
+const std::string VALID_NAME_REGEX = "[a-z][a-z_0-9]+";
+const std::string VALID_PATH_REGEX = "(.*/O2/.*)|(.*/test/.*)";
 
-const std::string VALID_NAME_REGEX = "([a-z]+_)*[a-z]+";
+bool isOutsideOfTargetScope(std::string filename)
+{
+  return !std::regex_match(filename, std::regex(VALID_PATH_REGEX));
+}
 
 void NamespaceNamingCheck::registerMatchers(MatchFinder *Finder) {
   const auto validNameMatch = matchesName( std::string("::") + VALID_NAME_REGEX + "$" );
   
   // matches namespace declarations that have invalid name
-  Finder->addMatcher(namespaceDecl( unless( validNameMatch ) ).bind("namespace-decl"), this);
+  Finder->addMatcher(namespaceDecl(allOf(
+    unless(validNameMatch),
+    unless(isAnonymous())
+    )).bind("namespace-decl"), this);
   // matches usage of namespace
-  Finder->addMatcher(nestedNameSpecifierLoc(loc(nestedNameSpecifier(specifiesNamespace(
-    unless( validNameMatch ) )))).bind("namespace-usage"), this );
+  Finder->addMatcher(nestedNameSpecifierLoc(loc(nestedNameSpecifier(specifiesNamespace(unless(validNameMatch)
+    )))).bind("namespace-usage"), this );
   // matches "using namespace" declarations
-  Finder->addMatcher(usingDirectiveDecl().bind("using-namespace"), this);
+  Finder->addMatcher(usingDirectiveDecl(unless(isImplicit()
+    )).bind("using-namespace"), this);
 }
 
 void NamespaceNamingCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedNamespaceDecl = Result.Nodes.getNodeAs<NamespaceDecl>("namespace-decl");
   if( MatchedNamespaceDecl )
   {
+    if( isOutsideOfTargetScope( Result.SourceManager->getFilename(MatchedNamespaceDecl->getLocation()).str() ) )
+    {
+      return;
+    }
+  
     std::string newName(MatchedNamespaceDecl->getDeclName().getAsString());
     
     fixNamespaceName(newName);
@@ -51,6 +66,10 @@ void NamespaceNamingCheck::check(const MatchFinder::MatchResult &Result) {
   if( MatchedNamespaceLoc )
   {
     const auto *AsNamespace = MatchedNamespaceLoc->getNestedNameSpecifier()->getAsNamespace();
+    if( isOutsideOfTargetScope( Result.SourceManager->getFilename(AsNamespace->getLocation()).str() ) )
+    {
+      return;
+    }
     std::string newName(AsNamespace->getDeclName().getAsString());
     
     fixNamespaceName(newName);
@@ -63,6 +82,11 @@ void NamespaceNamingCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedUsingNamespace = Result.Nodes.getNodeAs<UsingDirectiveDecl>("using-namespace");
   if( MatchedUsingNamespace )
   {
+    if( isOutsideOfTargetScope( Result.SourceManager->getFilename(MatchedUsingNamespace->getNominatedNamespace()->getLocation()).str() ) )
+    {
+      return;
+    }
+    
     std::string newName(MatchedUsingNamespace->getNominatedNamespace()->getDeclName().getAsString());
     
     if( std::regex_match(newName, std::regex(VALID_NAME_REGEX)) )
@@ -80,9 +104,9 @@ void NamespaceNamingCheck::check(const MatchFinder::MatchResult &Result) {
 
 void NamespaceNamingCheck::fixNamespaceName(std::string &name)
 {
-  for(int i=0; i<name.size(); i++) {
+  for(int i=name.size()-1; i>=0; i--) {
     if(isupper(name[i])) {
-      if(i != 0 && name[i-1] != '_') {
+      if(i != 0 && islower(name[i-1])) {
         name.insert(i, "_");
         i++;
       }
