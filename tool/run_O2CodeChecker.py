7bd8706d96405f6b21a52f995bfafac3876fc6da
Dario Berzano <dario.berzano@gmail.com>
2019-04-18 14:08:16 +0200

    Make it Python 3-compatible (#29)

--- tool/run_O2CodeChecker.py
+++ tool/run_O2CodeChecker.py
@@ -34,11 +34,15 @@ Compilation database setup:
 http://clang.llvm.org/docs/HowToSetupToolingForLLVM.html
 """
 
+from __future__ import print_function
 import argparse
 import json
 import multiprocessing
 import os
-import Queue
+try:
+  import Queue
+except ImportError:
+  import queue as Queue
 import re
 import shutil
 import subprocess
@@ -52,7 +56,7 @@ def find_compilation_database(path):
   result = './'
   while not os.path.isfile(os.path.join(result, path)):
     if os.path.realpath(result) == '/':
-      print 'Error: could not find compilation database.'
+      print('Error: could not find compilation database.')
       sys.exit(1)
     result += '../'
   return os.path.realpath(result)
@@ -158,9 +162,9 @@ def main():
     if args.checks:
       invocation.append('-checks=' + args.checks)
     invocation.append('-')
-    print subprocess.check_output(invocation)
+    print(subprocess.check_output(invocation))
   except:
-    print >>sys.stderr, "Unable to run clang-tidy."
+    print("Unable to run clang-tidy.")
     sys.exit(1)
 
   # Load the database and extract all files.
@@ -198,13 +202,13 @@ def main():
   except KeyboardInterrupt:
     # This is a sad hack. Unfortunately subprocess goes
     # bonkers with ctrl-c and we start forking merrily.
-    print '\nCtrl-C detected, goodbye.'
+    print('\nCtrl-C detected, goodbye.')
     if args.fix:
       shutil.rmtree(tmpdir)
     os.kill(0, 9)
 
   if args.fix:
-    print 'Applying fixes ...'
+    print('Applying fixes ...')
     apply_fixes(args, tmpdir)
 
 if __name__ == '__main__':
