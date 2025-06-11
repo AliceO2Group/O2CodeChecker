476e5350b46a9e7aa90693e96030f267298b99ef
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-06-11 10:42:24 +0200

    Use Clang provided public API
    - Drop our own private copy of the headers
    - Use the CMake Config
    
    ***

--- aliceO2/MemberNamesCheck.h
+++ aliceO2/MemberNamesCheck.h
@@ -10,8 +10,8 @@
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_ALICEO2_MEMBER_NAMES_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_ALICEO2_MEMBER_NAMES_H
 
-#include "../ClangTidy.h"
-#include "../ClangTidyCheck.h"
+#include "clang-tidy/ClangTidy.h"
+#include "clang-tidy/ClangTidyCheck.h"
 #include <regex>
 
 namespace clang {
--- aliceO2/SizeofCheck.h
+++ aliceO2/SizeofCheck.h
@@ -10,8 +10,8 @@
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_ALICEO2_SIZEOF_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_ALICEO2_SIZEOF_H
 
-#include "../ClangTidy.h"
-#include "../ClangTidyCheck.h"
+#include "clang-tidy/ClangTidy.h"
+#include "clang-tidy/ClangTidyCheck.h"
 
 namespace clang {
 namespace tidy {
